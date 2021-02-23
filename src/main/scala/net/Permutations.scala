401292a5d138762afb6c4119df86a68364ff6b47
Kevin Meredith <kevin.m.meredith@gmail.com>
2021-02-22 23:12:55 -0500

    improve.

--- src/main/scala/net/Permutations.scala
+++ src/main/scala/net/Permutations.scala
@@ -4,14 +4,28 @@ import scala.annotation.tailrec
 
 object Permutations {
 
-  private def factorial(n: Int): BigDecimal =
-    factorialHelper(n, 1)
+  def permutations[A](str: List[A]): List[List[A]] =
+    str match {
+      case Nil => List(Nil)
+      case l @ _ :: _ => permutationsHelper(l)
+    }
 
-  @tailrec
-  private def factorialHelper(n: Int, acc: BigDecimal): BigDecimal = {
-    if (n <= 1) acc
-    else factorialHelper(n - 1, acc * n)
-  }
+  private def permutationsHelper[A](str: List[A]): List[List[A]] =
+    str match {
+      case Nil => Nil
+      case a :: b :: Nil => List(List(a, b), List(b, a))
+      case list @ _ :: _ =>
+        val shifteds: List[List[A]] =
+          shiftN(list, list.length)
+
+        shifteds.flatMap {
+          case head :: tail =>
+            permutations(tail).map { lists: List[A] =>
+              head :: lists
+            }
+          case Nil => Nil
+        }
+    }
 
   private def shiftN[A](list: List[A], n: Int): List[List[A]] = {
     if (n <= 0) Nil
@@ -25,35 +39,4 @@ object Permutations {
     case head :: tail => tail ++ List(head)
     case Nil => Nil
   }
-
-  //  private def shiftHelper[A](n: Int, list: List[A], acc: List[List[A]]): List[List[A]] = {
-  //    if(n <= 0) acc
-  //    else {
-  //      val shifted: List[A] = shift(list)
-  //      shiftHelper(n-1, shifted, shifted :: acc)
-  //    }
-
-  def permutations[A](str: List[A]): List[List[A]] =
-    str match {
-      case Nil => List(Nil)
-      case l @ head :: tail => permutationsHelper(l)
-    }
-
-  def permutationsHelper[A](str: List[A]): List[List[A]] =
-    str match {
-      case Nil => Nil
-      case head :: Nil => List(List(head))
-      case a :: b :: Nil => List(List(a, b), List(b, a))
-      case list@head :: tail =>
-        val shifteds: List[List[A]] =
-          shiftN(list, list.length)
-
-        shifteds.flatMap {
-          case head :: tail =>
-            permutations(tail).map { lists: List[A] =>
-              head :: lists
-            }
-          case Nil => Nil
-        }
-    }
 }
